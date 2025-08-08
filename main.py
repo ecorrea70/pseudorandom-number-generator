d9f77721ba246cd72e5e508594291c1d95ed2ec3
ecorrea70 <eduardodasilvacorrea70@gmail.com>
2025-08-07 21:21:41 -0300

    generate pseudorandom numbers

--- main.py
+++ main.py
@@ -1,1 +1,13 @@
-seed = 1
+a = 1664525
+c = 29381213
+m = 60000000000
+x0 = 522
+
+with open('generated_numbers.csv', 'w') as file:
+    for i in range(1000):
+        x = (a * x0 + c) % m
+        uniform = x / m
+        file.write(f"{i+1};{uniform:.10f}".replace('.', ',') + '\n')
+        x0 = x
+
+print("file 'generated_numbers.csv' created successfully!")
